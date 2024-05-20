8b59e18db5be8aa6dd723586af6470e7b797eb0b
0gopi0 <gopivenkatcool@gmail.com>
2024-05-21 01:54:35 +0530

    Displaying critic score

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/robo.svg";
+import logo from "../assets/logo3.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 const NavBar = () => {
   return (
